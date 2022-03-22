2df4b4d3ae4711cb7cf9c791c2897f6e606ebcb0
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-03-22 14:54:00 +0100

    Minor

--- src/scripts/connect.py
+++ src/scripts/connect.py
@@ -1,6 +1,8 @@
 from operator import imod
+import re
 from neo4j import GraphDatabase
 from pandas import DataFrame
+from pyparsing import line
 
 class Neo4jConnection:
     """
@@ -57,14 +59,27 @@ class Neo4jConnection:
         """
         def __run_query(tx, query):
             values, result = list(), tx.run(query)
+            columns = list(result.keys())
             for _, record in enumerate(result):
                 value = record.values()
                 if not value: continue
                 values.append(value)
             _ = result.consume()
-            return values
+            return values, columns
         with self.__driver.session(database=self.__database) as session:
             __method = session.read_transaction if read_query else session.write_transaction
             __data = __method(__run_query, query)
-            return __data if raw else DataFrame(__data)
-            
+            columns=self._get_column_names(query=query)
+            return __data if raw else DataFrame(__data), columns
+            # return __data if raw else DataFrame(__data, columns=self._get_column_names(query=query))
+    
+    def _get_column_names(self, query):
+        if not isinstance(query, str): return
+
+        __lines = query.split("\n")
+        if not len(__lines): return
+
+        for __i, __line in enumerate(__lines[::-1]):
+            if __line.lower().find("return") == -1: continue
+            __return_string = "".join(__line[len(__lines) - __i - 1: len(__lines) - 1]).replace("  ", "").replace(", ", ",").replace(" ,", ",")
+            return __return_string.lower().replace("return ", '').split(",")
