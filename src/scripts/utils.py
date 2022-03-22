351473c70465e5ef69d59166b27ec15131085918
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-03-22 15:25:17 +0100

    Minor update

--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -4,12 +4,12 @@ def run_query(driver, query, **kwargs):
     """
     Basic wrapper around 'driver' object
     """
-    if not isinstance(driver, Neo4jConnection) or not isinstance(query, str): return
+    if (not isinstance(driver, Neo4jConnection)) or (not isinstance(query, str)): return
     return driver.query(query, **kwargs)
 
 def run_query_with_result(driver, query, **kwargs):
     """
     Basic wrapper around 'driver' object
     """
-    if not isinstance(driver, Neo4jConnection) or not isinstance(query, str): return
+    if (not isinstance(driver, Neo4jConnection)) or (not isinstance(query, str)): return
     return driver.query_with_result(query, **kwargs)
